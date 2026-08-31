93c5a576d4ac388f588b4b28b16c30e273285061
Sudip4262 <paul.sudip.ind@gmail.com>
2026-08-31 17:43:04 +0530

    .

--- backend/Models/HomeBannerSchema.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const HomePageBanner = new mongoose.Schema({
-    banners:[String]
-})
-
-const homebanners = mongoose.model("homebanners", HomePageBanner )
-module.exports = homebanners
